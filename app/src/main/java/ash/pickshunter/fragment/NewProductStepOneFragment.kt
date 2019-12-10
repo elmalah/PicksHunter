d879e258aea8711ef27718d5112eb156a06634c3
aelmalah <elmalah.soft@gmail.com>
2019-12-10 21:41:02 +0200

    commit implement multiple images

--- app/src/main/java/ash/pickshunter/fragment/NewProductStepOneFragment.kt
+++ app/src/main/java/ash/pickshunter/fragment/NewProductStepOneFragment.kt
@@ -155,29 +155,31 @@ class NewProductStepOneFragment : Fragment() {
 
         iv_product.setOnClickListener {
             //check runtime permission
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                    ) ==
-                    PackageManager.PERMISSION_DENIED
-                ) {
-                    //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    //show popup to request runtime permission
-                    requestPermissions(permissions,
-                        PERMISSION_CODE
-                    );
-                } else {
-                    //permission already granted
-                    pickImageFromGallery();
-                }
-            } else {
-                //system OS is < Marshmallow
-                pickImageFromGallery();
-            }
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                if (checkSelfPermission(
+//                        requireContext(),
+//                        Manifest.permission.READ_EXTERNAL_STORAGE
+//                    ) ==
+//                    PackageManager.PERMISSION_DENIED
+//                ) {
+//                    //permission denied
+//                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+//                    //show popup to request runtime permission
+//                    requestPermissions(permissions,
+//                        PERMISSION_CODE
+//                    );
+//                } else {
+//                    //permission already granted
+//                    pickImageFromGallery();
+//                }
+//            } else {
+//                //system OS is < Marshmallow
+//                pickImageFromGallery();
+//            }
 
-
+            val bundle = Bundle()
+            bundle.putParcelable("shop", shop)
+            NavHostFragment.findNavController(main_navigation).navigate(R.id.fragment_new_product_select_images, bundle)
         }
 
         bt_add_product.setOnClickListener {
---
+++ app/src/main/java/ash/pickshunter/fragment/PickProductImagesFragment.kt
@@ -0,0 +1,350 @@
+package ash.pickshunter.fragment
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import com.fly365.utils.injection.InjectorUtils
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_new_product_step_one.*
+import android.widget.AdapterView
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.navigation.fragment.NavHostFragment
+import ash.pickshunter.*
+import ash.pickshunter.model.*
+import ash.pickshunter.utils.ProgressDialog
+import ash.pickshunter.viewModel.TripViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_new_product_select_images.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [NewProductStepOneFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [NewProductStepOneFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PickProductImagesFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var shop: Shop? = null
+    private var categoryId: Int = 0
+    private var pictureId: Int? = null
+
+    private val viewModel: TripViewModel by viewModels {
+        InjectorUtils.provideTripViewModelFactory(requireContext())
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+        shop = arguments?.getParcelable("shop")
+        //shop = Shop()
+        //shop!!.id = 85
+        //shop!!.tripShopId = 85
+        //shop!!.name = "dds"
+
+    }
+
+    private fun takePhotoFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, PERMISSION_CAMERA_CODE)
+    }
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,
+            IMAGE_PICK_CODE
+        )
+    }
+
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup granted
+                    pickImageFromGallery()
+                } else {
+                    //permission from popup denied
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        when (requestCode) {
+            PERMISSION_CAMERA_CODE  -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    takePhotoFromCamera()
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == IMAGE_PICK_CODE)
+        {
+
+
+            if (data != null)
+            {
+                val contentURI = data!!.data
+                try
+                {
+                    val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, contentURI)
+                    val path = saveImage(bitmap)
+                    Toast.makeText(requireContext(), "Image Saved!", Toast.LENGTH_SHORT).show()
+                    iv_preview!!.setImageBitmap(bitmap)
+
+                    ProgressDialog.show(requireContext(), false)
+
+                    val pictureRequest = PictureRequest()
+                    pictureRequest.file = File(getRealPathFromUri(requireContext(), data?.data!!))
+                    pictureRequest.fileUri = data?.data.toString()
+
+                    viewModel.addPicture(pictureRequest).observe(this) {
+                        ProgressDialog.dismiss()
+                        pictureId = it.PictureId
+                    }
+
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(requireContext(), "Failed!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+        else if (requestCode == PERMISSION_CAMERA_CODE)
+        {
+            val thumbnail = data!!.extras!!.get("data") as Bitmap
+            iv_preview!!.setImageBitmap(thumbnail)
+            saveImage(thumbnail)
+            Toast.makeText(requireContext(), "Image Saved!", Toast.LENGTH_SHORT).show()
+
+            val pictureRequest = PictureRequest()
+            pictureRequest.file = File(getRealPathFromUri(requireContext(), data?.data!!))
+            pictureRequest.fileUri = data?.data.toString()
+
+            viewModel.addPicture(pictureRequest).observe(this) {
+                ProgressDialog.dismiss()
+                pictureId = it.PictureId
+            }
+        }
+    }
+    private fun saveImage(myBitmap: Bitmap):String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+        val wallpaperDirectory = File(
+            (Environment.getExternalStorageDirectory()).toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+        Log.d("fee",wallpaperDirectory.toString())
+        if (!wallpaperDirectory.exists())
+        {
+
+            wallpaperDirectory.mkdirs()
+        }
+
+        try
+        {
+            Log.d("heel",wallpaperDirectory.toString())
+            val f = File(wallpaperDirectory, ((Calendar.getInstance()
+                .getTimeInMillis()).toString() + ".jpg"))
+            f.createNewFile()
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(requireContext(),
+                arrayOf(f.getPath()),
+                arrayOf("image/jpeg"), null)
+            fo.close()
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath())
+
+            return f.getAbsolutePath()
+        }
+        catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+    }
+    private fun getRealPathFromUri(context: Context, contentUri: Uri): String {
+        var cursor: Cursor? = null
+        try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null)
+            val column_index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor!!.moveToFirst()
+            return cursor!!.getString(column_index)
+        } finally {
+            if (cursor != null) {
+                cursor!!.close()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_product_select_images, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ln_gallery.setOnClickListener {
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions,
+                        PERMISSION_CODE
+                    );
+                } else {
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            } else {
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+
+
+        }
+
+       iv_camera.setOnClickListener {
+           //if system os is Marshmallow or Above, we need to request runtime permission
+           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+               if (checkSelfPermission(requireContext(),Manifest.permission.CAMERA)
+                   == PackageManager.PERMISSION_DENIED ||
+                   checkSelfPermission(requireContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                   == PackageManager.PERMISSION_DENIED  || checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED
+               ){
+
+                   //permission was not enabled
+                   val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+                   //show popup to request permission
+                   requestPermissions(permission, PERMISSION_CAMERA_CODE)
+
+
+               }
+               else{
+                   //permission already granted
+                   takePhotoFromCamera()
+               }
+           }
+           else{
+               //system os is < marshmallow
+               takePhotoFromCamera()
+           }
+       }
+
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment NewProductStepOneFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            PickProductImagesFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+        private val PERMISSION_CAMERA_CODE = 1003;
+
+        var image_uri: Uri? = null
+        private val IMAGE_DIRECTORY = "/picks"
+    }
+}
